95c7cba8a30629abdb8ff5ab795e630849269421
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-11-17 14:33:59 +0400

    Refactor project structure and add Tailwind CSS support

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "../global.css";
+
+export default function App() {
+  return (
+    <View>
+      <Text className='mt-10 text-red-400'>Open up App.tsx frllo to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
