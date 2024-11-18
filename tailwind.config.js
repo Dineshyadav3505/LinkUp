dbbdca124c37b6cd15adaf8689b01d12ce90a856
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-11-18 14:33:59 +0400

    Update dependencies and configuration for react-native-svg and react-native-vector-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,14 +27,6 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
-      radius: {
-        "xs": 10,
-        "sm": 12,
-        "md": 14,
-        "lg": 16,
-        "xl": 18,
-        "xxl": 22,
-      },
     },
   },
   plugins: [],
